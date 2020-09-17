5c12687c1e7985edf5ddaa5af332b719d12b6321
Matt Bowman <mbowman@yext.com>
2020-09-17 13:41:57 -0400

    Make apk output verbose

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add --no-cache ruby g++ gcc make musl-dev
+apk add --no-cache -v ruby g++ gcc make musl-dev
 gem install bundler --no-ri --no-rdoc
 bundle install
 bundle exec middleman init
