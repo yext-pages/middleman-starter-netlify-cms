760c3dfe79327e6d9dca346a442b16aedd347792
Sugat Poudel <spoudel@yext.com>
2020-11-16 20:28:28 -0500

    removed version flag from apt

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-apt update
-apt install -y -v ruby g++ gcc make musl-dev
+apt install -y ruby g++ gcc make musl-dev
 gem install bundler --no-ri --no-rdoc
 bundle install
 bundle exec middleman init
