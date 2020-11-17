2238066f7581538d7b676a832044e806345d90de
Sugat Poudel <spoudel@yext.com>
2020-11-16 20:33:50 -0500

    installing ruby-dev instead of ruby

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt install -y ruby g++ gcc make musl-dev
+apt install -y ruby-dev g++ gcc make musl-dev
 gem install bundler:1.17.3 --no-ri --no-rdoc
 bundle install
 bundle exec middleman init
